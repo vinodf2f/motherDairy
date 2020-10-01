8ace3bf67b917c14cae2543ebfc6116d9f91c87e
vinodf2f <vinodyevatikar95@gmail.com>
2020-10-01 11:12:22 +0530

    build folder

---
+++ build/precache-manifest.703680e357aec10fd507c83275f56088.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4795bd1302cb4323ba0",
+    "url": "%7BGithub-username%7D.github.io/%7BGithub-repo-name%7D/static/js/runtime~main.e4795bd1.js"
+  },
+  {
+    "revision": "408d313a6002a735f731",
+    "url": "%7BGithub-username%7D.github.io/%7BGithub-repo-name%7D/static/js/main.408d313a.chunk.js"
+  },
+  {
+    "revision": "e9403118f2cad6d5021d",
+    "url": "%7BGithub-username%7D.github.io/%7BGithub-repo-name%7D/static/js/2.e9403118.chunk.js"
+  },
+  {
+    "revision": "408d313a6002a735f731",
+    "url": "%7BGithub-username%7D.github.io/%7BGithub-repo-name%7D/static/css/main.28b4bd40.chunk.css"
+  },
+  {
+    "revision": "f18f4dea1cf9c2bb00fd41d1ecf02e76",
+    "url": "%7BGithub-username%7D.github.io/%7BGithub-repo-name%7D/index.html"
+  }
+];
